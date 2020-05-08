019c51f2694dd35bd3a88c90d371642e0b473295
jarvis-666 <vm897@snu.edu.in>
2020-05-08 15:07:51 +0530

    Updated interface for symmetric widgets

--- alarm.py
+++ alarm.py
@@ -1,31 +1,25 @@
 from tkinter import *
-import datetime
+import time
 
 root = Tk()
 root.title("Alarm Clock")
 
-main_frame = LabelFrame(root, pady=0, padx=0)
-title_label = Label(main_frame, font='bold', text="ALARM")
-set_label = Label(main_frame, text="Time to set the alarm for: ")
-enter_time = Entry(main_frame, width=50, borderwidth=5)
-alarm_set_time = datetime.time()
+main_frame = LabelFrame(root)
+alarm_state = 'OFF'
+top_label = Label(main_frame, text=f"ALARM {alarm_state}")
+set_alarm_label = Label(main_frame, text="Time to set alarm for:")
+time_entry = Entry(main_frame)
 set_alarm_button = Button(main_frame, text="Set Alarm")
-clear_button = Button(main_frame, text="Clear")
-alarm_set_label = Label(main_frame, text="Alarm Set")
-
-alarm_frame = LabelFrame(root)
-display_label = Label(alarm_frame, text="Time to Wake Up")
-stop_button = Button(alarm_frame, text="Stop")
+time_entered = time.asctime(time.localtime())
+clear_alarm_button = Button(main_frame, text="Clear")
+status_label = Label(main_frame, text=f"Alarm set for {time_entered}", bd=1, relief=SUNKEN)
 
 main_frame.grid(row=0, column=0)
-title_label.grid(row=0, column=0, pady=10)
-set_label.grid(row=1, column=0, pady=10)
-enter_time.grid(row=1, column=1, pady=10)
+top_label.grid(row=0, column=2, padx=10, pady=10)
+set_alarm_label.grid(row=1, column=0, padx=10)
+time_entry.grid(row=1, column=3)
 set_alarm_button.grid(row=2, column=0, pady=10)
-clear_button.grid(row=2, column=1, pady=10)
-alarm_set_label.grid(row=3, column=0, columnspan=3, pady=10)
+clear_alarm_button.grid(row=2, column=3, padx=10, pady=10)
+status_label.grid(row=3, column=2)
 
-alarm_frame.grid(row=1, column=0, pady=50, sticky=W+E)
-display_label.grid(row=0, column=0, padx=180)
-stop_button.grid(row=1, column=0, pady=10)
 root.mainloop()
