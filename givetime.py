57c4bf9778fdc750a15cf88f04f9d9b72e0fc7a1
jarvis-666 <vm897@snu.edu.in>
2020-05-21 20:28:16 +0530

    Alarm GUI ready

---
+++ givetime.py
@@ -0,0 +1,15 @@
+import datetime
+
+h, m = map(int, input("Set time for alarm: ").split())
+alarm_set_time = datetime.time(h, m)
+
+print(f"Alarm set for {alarm_set_time}")
+
+date_today = datetime.date.today()
+
+full_info = datetime.datetime.combine(date_today, alarm_set_time)
+
+print(full_info)
+
+status_ready = datetime.datetime.strptime(str(full_info), '%Y-%m-%d %H:%M:%S').strftime('%a %b %d %I:%M:%S %Y')
+print(status_ready)
