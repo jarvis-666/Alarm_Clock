5756f01ea4e8ab66a801bb3cdd6f8d66f86f0de7
jarvis-666 <vm897@snu.edu.in>
2020-05-26 20:42:08 +0530

    Added functionality to update clock for monitoring time

--- alarm.py
+++ alarm.py
@@ -5,32 +5,38 @@ import datetime
 import winsound
 
 alarm_set_time = None
+status_label = None
 
-def button_pop():
-    response = messagebox.showinfo(title="Wake Up!", message="Hey! It's time to wake up...")
-    if response == 'ok':
-       Label(root, text="Alarm OFF").pack()
-    # Label(root, text=response).pack()
+def update_clock():
+    current_time = str(datetime.datetime.now().time())
+    if current_time >= str(alarm_set_time):
+        playAlarm()
+        return
+    else:
+        root.after(1000, func=update_clock)
+
 
 def playAlarm():
-    stop = False
-    while not stop:
-        current_time = str(datetime.datetime.now().time())
-        if current_time >= str(alarm_set_time):
-            stop = True
-
     winsound.PlaySound(sound="C:\\Users\\Vin\\Downloads\\Alarm.wav",
                        flags=winsound.SND_FILENAME | winsound.SND_ASYNC | winsound.SND_LOOP)
-    messagebox.showinfo(title="Wake Up!", message="Hey! It's time to wake up...")
-    time.sleep(10)
-    winsound.PlaySound(sound="C:\\Users\\Vin\\Downloads\\Alarm.wav", flags=winsound.SND_PURGE)
+    response = messagebox.showinfo(title="Wake Up!", message="Hey! It's time to wake up...")
+    if response == 'ok':
+        global status_label
+        status_label.destroy()
+        winsound.PlaySound(sound="C:\\Users\\Vin\\Downloads\\Alarm.wav", flags=winsound.SND_PURGE)
+        alarm_state = 'OFF'
+        top_label.config(text=f"ALARM {alarm_state}")
+        status_label = Label(main_frame, text="Alarm not set", bd=1, relief=SUNKEN)
+        status_label.grid(row=3, column=2)
+        alarm_set_time = None
 
 
 def set_alarm():
+    global alarm_set_time
+    global status_label
     alarm_set_time = time_entry.get()
     alarm_set_time = alarm_set_time.split(' ')
     alarm_set_time = datetime.time(int(alarm_set_time[0]), int(alarm_set_time[1]))
-    Label(root, text=f"{alarm_set_time}").pack()
     alarm_state = "ON"
     top_label.config(text=f"ALARM {alarm_state}")
     date_today = datetime.date.today()
@@ -38,33 +44,32 @@ def set_alarm():
     status_ready = datetime.datetime.strptime(str(full_info), '%Y-%m-%d %H:%M:%S').strftime('%a %b %d %I:%M:%S %p %Y')
     status_label = Label(main_frame, text=f"Alarm set for {status_ready}", bd=1, relief=SUNKEN)
     status_label.grid(row=3, column=2)
-    playAlarm()
+    update_clock()
 
 
 def clear_alarm():
     time_entry.delete(0, END)
 
+if __name__ == '__main__':
+    root = Tk()
+    root.title("Alarm Clock")
 
-root = Tk()
-root.title("Alarm Clock")
+    main_frame = LabelFrame(root)
+    alarm_state = 'OFF'
+    top_label = Label(main_frame, text=f"ALARM {alarm_state}")
+    set_alarm_label = Label(main_frame, text="Time to set alarm for:")
+    time_entry = Entry(main_frame)
+    set_alarm_button = Button(main_frame, text="Set Alarm", command=set_alarm)
+    clear_alarm_button = Button(main_frame, text="Clear", command=clear_alarm)
+    status_label = Label(main_frame, text="Alarm not set", bd=1, relief=SUNKEN)
 
-main_frame = LabelFrame(root)
-alarm_state = 'OFF'
-top_label = Label(main_frame, text=f"ALARM {alarm_state}")
-set_alarm_label = Label(main_frame, text="Time to set alarm for:")
-time_entry = Entry(main_frame)
-set_alarm_button = Button(main_frame, text="Set Alarm", command=set_alarm)
-clear_alarm_button = Button(main_frame, text="Clear", command=clear_alarm)
-status_label = Label(main_frame, text="Alarm not set", bd=1, relief=SUNKEN)
-demo_button = Button(root, text="demo", command = button_pop)               ## This is temporary
+    main_frame.pack()
+    top_label.grid(row=0, column=2, padx=10, pady=10)
+    set_alarm_label.grid(row=1, column=0, padx=10)
+    time_entry.grid(row=1, column=3)
+    set_alarm_button.grid(row=2, column=0, pady=10)
+    clear_alarm_button.grid(row=2, column=3, padx=10, pady=10)
+    status_label.grid(row=3, column=2)
 
-main_frame.pack()
-top_label.grid(row=0, column=2, padx=10, pady=10)
-set_alarm_label.grid(row=1, column=0, padx=10)
-time_entry.grid(row=1, column=3)
-set_alarm_button.grid(row=2, column=0, pady=10)
-clear_alarm_button.grid(row=2, column=3, padx=10, pady=10)
-status_label.grid(row=3, column=2)
-demo_button.pack()
-
-root.mainloop()
+    root.update_idletasks()
+    root.mainloop()
